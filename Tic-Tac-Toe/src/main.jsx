c9aafa06291b7f65091232ba2d480406a2307198
viktorriznik73-blip <viktorriznik73@gmail.com>
2026-07-14 16:21:14 +0200

    Styles fixing

--- Tic-Tac-Toe/src/main.jsx
+++ Tic-Tac-Toe/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root');
+
+if (container) {
+  createRoot(container).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+} else {
+  console.error("Ошибка: Элемент с id 'root' не найден в HTML!");
+}
